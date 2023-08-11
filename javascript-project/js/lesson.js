ecbcecfd0767878ee5a2aced0c4fcd625426e7e4
Markovka <116700521+mmorkkovkaa@users.noreply.github.com>
2023-08-11 22:29:56 +0600

    uploaded the project

---
+++ javascript-project/js/lesson.js
@@ -0,0 +1,19 @@
+/// phone checker
+
+const phoneInput = document.querySelector('#phone_input')
+const phoneCheck = document.querySelector('#phone_button')
+const phoneResult = document.querySelector('#phone_result')
+
+
+const regExp = /^\+996 \d{3} \d{2}-\d{2}-\d{2}$/
+
+phoneCheck.onclick = () =>{
+    if (regExp.test(phoneInput.value)) {
+        phoneResult.innerHTML = 'ok'
+        phoneResult.style.color = 'green'
+    }else {
+        phoneResult.innerHTML = 'not ok'
+        phoneResult.style.color = 'red'
+    }
+}
+
